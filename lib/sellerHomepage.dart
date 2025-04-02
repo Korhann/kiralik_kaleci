233d3907e9b91f621867c5b62b8ee810ed23a918
Korhan <korhandemir1@gmail.com>
2025-04-02 20:41:03 +0300

    ..yeahahahhah

--- lib/sellerHomepage.dart
+++ lib/sellerHomepage.dart
@@ -3,10 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:kiralik_kaleci/appointmentspage.dart';
-import 'package:kiralik_kaleci/globals.dart';
 import 'package:kiralik_kaleci/selleraddpage.dart';
-import 'package:kiralik_kaleci/sellermainpage.dart';
-import 'package:kiralik_kaleci/settingsMenu.dart';
 import 'package:kiralik_kaleci/styles/colors.dart';
 
 class SellerHomePage extends StatefulWidget {
@@ -17,9 +14,7 @@ class SellerHomePage extends StatefulWidget {
 }
 
 class _SellerHomePageState extends State<SellerHomePage> {
-
   String currentUser = FirebaseAuth.instance.currentUser!.uid;
-  bool userisSeller = false;
 
   @override
   Widget build(BuildContext context) {
@@ -49,10 +44,9 @@ class _SellerHomePageState extends State<SellerHomePage> {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 const SizedBox(height: 30),
-                SellerQuickMenus(text: 'İlanı Düzenle', ontap: () => _navigateToPage('sellerAdd'), icon: Icons.add),
+                SellerQuickMenus(text: 'İlan Ekle/Düzenle', ontap: () => _navigateToPage('sellerAdd'), icon: Icons.add),
                 const SizedBox(height: 10),
-                // todo: Bunu da öbürleriyle aynı şekilde yap
-                noOfAppointments(onTap: () => _navigateToPage('appointments')),
+                NoOfAppointments(onTap: () => _navigateToPage('appointments')),
                 const SizedBox(height: 10),
                 SellerQuickMenus(text: 'Kazançlarım Fiverr a bak', ontap: () => _navigateToPage('earnings'), icon: Icons.attach_money)
               ],
@@ -63,38 +57,6 @@ class _SellerHomePageState extends State<SellerHomePage> {
     );
   }
 
-  Widget buildButton() {
-  return FutureBuilder<bool>(
-    future: checkIfUserIsAlreadySeller(),
-    builder: (context, snapshot) {
-      if (snapshot.connectionState == ConnectionState.waiting) {
-        return CircularProgressIndicator();
-      }
-
-      if (snapshot.hasData) {
-        return Text(
-          snapshot.data! ? 'İlanı Düzenle' : 'İlan Ekle',
-          style: GoogleFonts.inter(
-            fontSize: 20,
-            fontWeight: FontWeight.w700,
-            color: Colors.white,
-          ),
-        );
-      }
-
-      return Text('Hata', style: GoogleFonts.inter(fontSize: 20));
-    },
-  );
-}
-Future<bool> checkIfUserIsAlreadySeller() async {
-  final documentSnapshot = await FirebaseFirestore.instance.collection('Users').doc(currentUser).get();
-  userisSeller = documentSnapshot.data()?['sellerDetails'] != null;
-  if (userisSeller) {
-    return true;
-  } else {
-    return false;
-  }
-  }
   void _navigateToPage(String menuOption) async {
     Widget page;
     switch (menuOption) {
@@ -132,7 +94,7 @@ class SellerQuickMenus extends StatelessWidget {
         child: Container(
           height: 50,
           width: MediaQuery.sizeOf(context).width,
-          color: userorseller ? sellergrey : Colors.white,
+          color: sellergrey,
           child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 20),
             child: Row(
@@ -142,13 +104,14 @@ class SellerQuickMenus extends StatelessWidget {
                   style: GoogleFonts.roboto(
                       fontSize: 18,
                       fontWeight: FontWeight.w400,
-                      color: userorseller ? Colors.white : Colors.black),
+                      color: Colors.white
+                    ),
                 ),
                 const Spacer(),
                 Icon(
                   icon,
                   size: 24,
-                  color: userorseller ? Colors.white : Colors.black,
+                  color: Colors.white,
                 )
               ],
             ),
@@ -159,62 +122,87 @@ class SellerQuickMenus extends StatelessWidget {
   }
 }
 
-class noOfAppointments extends StatelessWidget {
+class NoOfAppointments extends StatelessWidget {
   final VoidCallback onTap;
 
-  const noOfAppointments({super.key, required this.onTap});
+  const NoOfAppointments({super.key, required this.onTap});
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-            onTap: onTap,
-            child: Stack(
-              clipBehavior: Clip.none,
+      onTap: onTap,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(10),
+        child: Container(
+          height: 50,
+          width: MediaQuery.sizeOf(context).width,
+          color: sellergrey,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Row(
               children: [
-                Padding(
-                  padding: const EdgeInsets.only(right: 10),
-                  child: Icon(Icons.notifications, size: 24, color: Colors.white),
+                Text(
+                  "Randevularım",
+                  style: GoogleFonts.roboto(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.white,
+                  ),
                 ),
-                StreamBuilder<int>(
-                  stream: getUnreadCount(), // Fetch unread notifications
-                  builder: (context, snapshot) {
-                    if (!snapshot.hasData || snapshot.data == 0)
-                      return SizedBox();
-                    return Positioned(
-                      right: 5, // Adjust position
-                      top: -3,
-                      child: Container(
-                        padding: const EdgeInsets.all(5),
-                        decoration: const BoxDecoration(
-                          color: Colors.red,
-                          shape: BoxShape.circle,
-                        ),
-                        child: Text(
-                          snapshot.data.toString(),
-                          style: const TextStyle(
-                            color: Colors.white,
-                            fontSize: 12,
-                            fontWeight: FontWeight.bold,
+                const Spacer(),
+                Stack(
+                  clipBehavior: Clip.none,
+                  children: [
+                    Icon(
+                      Icons.notifications,
+                      size: 24,
+                      color: Colors.white,
+                    ),
+                    StreamBuilder<int>(
+                      stream: getUnreadCount(),
+                      builder: (context, snapshot) {
+                        if (!snapshot.hasData || snapshot.data == 0) return SizedBox();
+                        return Positioned(
+                          right: -5,
+                          top: -5,
+                          child: Container(
+                            padding: const EdgeInsets.all(5),
+                            decoration: const BoxDecoration(
+                              color: Colors.red,
+                              shape: BoxShape.circle,
+                            ),
+                            child: Text(
+                              snapshot.data.toString(),
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 12,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
                           ),
-                        ),
-                      ),
-                    );
-                  },
+                        );
+                      },
+                    ),
+                  ],
                 ),
               ],
             ),
-          );
+          ),
+        ),
+      ),
+    );
   }
+
   Stream<int> getUnreadCount() {
     final String currentUser = FirebaseAuth.instance.currentUser!.uid;
-
     return FirebaseFirestore.instance
-      .collection('Users')
-      .doc(currentUser)
-      .collection('appointmentseller')
-      .where('appointmentDetails.status', isEqualTo: 'pending')
-      .snapshots()
-      .map((snapshot) => snapshot.docs.length);
-  }  
+        .collection('Users')
+        .doc(currentUser)
+        .collection('appointmentseller')
+        .where('appointmentDetails.status', isEqualTo: 'pending')
+        .snapshots()
+        .map((snapshot) => snapshot.docs.length);
+  }
 }
+
 
